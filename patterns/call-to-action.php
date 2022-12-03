334d8254b018e326e0932698eab6b09c70732edb
Marvin Engelmann <git@marvinengelmann.email>
2022-12-03 13:59:46 +0100

    Initial block theme

---
+++ patterns/call-to-action.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Call to action
+ * Slug: insurance-broker-palatinate/call-to-action
+ * Categories: featured
+ */
+?>
+
+<!-- wp:paragraph -->
+<p>CAll to action</p>
+<!-- /wp:paragraph -->
---
+++ patterns/intro.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Intro
+ * Slug: insurance-broker-palatinate/intro
+ * Categories: featured
+ * Keywords: Intro
+ * Block Types: core/buttons
+ */
+?>
+
+<!-- wp:paragraph -->
+<p>Intro</p>
+<!-- /wp:paragraph -->
